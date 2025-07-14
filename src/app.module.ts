25b87b32d87473706f48a3375859a145e511ce5f
Đặng Trung Hiếu <dangtrunghieu@192.168.1.2>
2025-07-14 20:39:48 +0700

    feat: init source

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { OpenAiService } from './utils/openAI/service';
+
+@Module({
+  imports: [UsersModule, ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService, OpenAiService],
+  exports: [OpenAiService],
+})
+export class AppModule {}
